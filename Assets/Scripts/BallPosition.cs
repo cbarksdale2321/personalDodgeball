a1560a118ac167b14b391df4e8f116cc5eb89cf2
cbarksdale2321 <55416957+cbarksdale2321@users.noreply.github.com>
2020-10-05 22:43:32 -0500

    My Updated Version

---
+++ Assets/Scripts/BallPosition.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPosition : MonoBehaviour
+{
+    public bool holdingBall;
+    public float ballforce;
+    public Transform ballDest;
+
+
+    void Update()
+    {
+        if (Input.GetKey("e"))
+        {
+
+            holdingBall = true;
+            GetComponent<Rigidbody>().useGravity = false;
+            this.transform.position = ballDest.position;
+            this.transform.parent = GameObject.Find("BallPosition").transform;
+
+
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            holdingBall = true;
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * ballforce);
+        }
+
+        else
+        {
+            holdingBall = false;
+            this.transform.parent = null;
+
+            GetComponent<Rigidbody>().useGravity = true;
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Terrain")
+        {
+            GetComponent<Rigidbody>().Sleep();
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    private void OnMouseUp()
+    {
+
+
+
+        GetComponent<Rigidbody>().AddForce(Vector3.forward);
+    }
+}
---
+++ Assets/Scripts/HitDetection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDetection : MonoBehaviour
+{
+    public GameObject testDummy;
+    private void OnCollisionEnter(Collision hit)
+    {
+
+        if(hit.collider.tag == "Object")
+        {
+            Destroy(testDummy);
+
+        }
+
+    }
+
+
+}
